f157b7f544854c58cff0afc2b578e0624ee44b6e
Rich <rchrdholme@gmail.com>
2018-05-22 21:15:14 -0400

    my book layout

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -623,7 +623,7 @@ $(document).ready(function() {
       // if movie doesnt exist 
 		  }else{
         
-        $("#find").append("<img id='noMovie' src ='assets/images/noVideo.jpg' height = 40px width = 40px>")
+        $("#myPoster").append("<img id='noMovie' src ='assets/images/noVideo.jpg' height = 40px width = 40px>")
    		}
    			
 	  });
